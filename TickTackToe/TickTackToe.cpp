9a0c5874cb3eaab92d5c80fc927d780823b9a411
Dan <dantran2016@gmail.com>
2017-11-10 15:02:42 -0800

    Added Functions

--- TickTackToe/TickTackToe.cpp
+++ TickTackToe/TickTackToe.cpp
@@ -19,12 +19,57 @@ string spot8 = "8";
 string spot9 = "9";
 
 bool gameOver() {
-
-	return false;
+	if (spot1 == spot2 && spot2 == spot3)
+		return true;
+	else if (spot4 == spot5 && spot5 == spot6)
+		return true;
+	else if (spot6 == spot7 && spot8 == spot9)
+		return true;
+	else if (spot1 == spot4 && spot4 == spot7)
+		return true;
+	else if (spot2 == spot5 && spot5 == spot8)
+		return true;
+	else if (spot3 == spot6 && spot6 == spot9)
+		return true;
+	else if (spot1 == spot5 && spot5 == spot9)
+		return true;
+	else if (spot3 == spot5 && spot5 == spot7)
+		return true;
+	else
+		return false;
 }
 
-void changeSpot() {
-	
+void changeSpot(int value, string letter) {
+	if (value == 1 && spot1 == "1") {
+		spot1 = letter;
+	}
+	else if (value == 2 && spot2 == "2") {
+		spot2 = letter;
+	}
+	else if (value == 3 && spot3 == "3") {
+		spot3 = letter;
+	}
+	else if (value == 4 && spot4 == "4") {
+		spot4 = letter;
+	}
+	else if (value == 5 && spot5 == "5") {
+		spot5 = letter;
+	}
+	else if (value == 6 && spot6 == "6") {
+		spot6 = letter;
+	}
+	else if (value == 7 && spot7 == "7") {
+		spot7 = letter;
+	}
+	else if (value == 8 && spot8 == "8") {
+		spot8 = letter;
+	}
+	else if (value == 9 && spot9 == "9") {
+		spot9 = letter;
+	}
+	else {
+		cout << "The spot you entered does not exit or already has already been taken" << endl;
+	}
 }
 
 void drawTable() {
@@ -47,12 +92,20 @@ int main()
 
 	int p1, p2;
 
-	while (!gameOver) {
+	while (true) {
 
 		cout << "Player 1 it's your turn" << endl;
 		cin >> p1;
 		
+		changeSpot(p1, "X");
 
+		drawTable();
+
+		cout << "Player 2 it's your turn" << endl;
+		cin >> p2;
+		changeSpot(p2, "O");
+
+		drawTable();
 
 	}
 
